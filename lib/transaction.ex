14db1c4799fe946e9115f06bc929e06ed4034790
nandakishore15 <nandu15@live.com>
2018-11-25 18:04:42 -0500

    Add transaction

---
+++ lib/transaction.ex
@@ -0,0 +1,32 @@
+defmodule Transaction do
+    # defstruct to: nil,
+    #           from: nil,
+    #           amount: 0,
+    
+    def init(to, from, amount, priv_key) do
+        msg = get_message(to, from, amount)
+        signature = sign(msg, priv_key)
+        signature
+    end
+
+    def get_message(to, from, amount) do
+        #concat and sign
+        concat = to <> from <> Integer.to_string(amount)
+        :crypto.hash(:sha256, concat) |> Base.encode16
+    end
+
+    def sign(msg, priv_key) do
+        {:ok, signature} = RsaEx.sign(msg, priv_key)
+        signature
+    end
+
+    def check_valid_signature(to, from, amount, signature, pub_key) do
+        msg = get_message(to, from, amount) #Not sure if we need to get message here.
+        if from != pub_key do
+            err_msg = "One can only create transactions from their wallet."
+            {:error, err_msg}
+        else
+            {:ok, valid} = RsaEx.verify(msg, signature, pub_key)
+        end
+    end
+end
