39b490b5377de78346507d829859ff01be017251
nandakishore15 <nandu15@live.com>
2018-11-26 17:35:44 -0500

    Add readme and minor change to wallet

--- lib/wallet.ex
+++ lib/wallet.ex
@@ -1,12 +1,12 @@
 defmodule Wallet do
     defstruct priv_key: nil,
-              public_key: nil
+              pub_key: nil
     
     def init() do
         {:ok, sk} = RsaEx.generate_private_key
         {:ok, pk} = RsaEx.generate_public_key(sk)
 
-        %Wallet{priv_key: sk, public_key: pk}
+        %Wallet{priv_key: sk, pub_key: pk}
     end
 
     def get_balance() do
