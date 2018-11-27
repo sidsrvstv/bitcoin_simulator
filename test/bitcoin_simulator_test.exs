d7ea4e2e8beb53c65d8d37971499c986a1c79711
nandakishore15 <nandu15@live.com>
2018-11-26 22:32:44 -0500

    Add back test

--- test/bitcoin_simulator_test.exs
+++ test/bitcoin_simulator_test.exs
@@ -6,60 +6,86 @@ defmodule BitcoinSimulatorTest do
     {:ok, server: blockchain}
   end
 
-  # test "add transactions and check length of blockchain" do
-  #   tx1 = ["from: Alice, to: Bob, amount = 10"]
-  #   tx2 = ["seller: Alice, buyer: Charlie, amount = 20"]
-  #   tx3 = ["seller: Bob, buyer: Charlie, amount = 15"]
+  test "add transactions and check length of blockchain" do
+    {:ok, bob_sk} = RsaEx.generate_private_key
+    {:ok, bob_pk} = RsaEx.generate_public_key(bob_sk)
 
-  #   {:ok, block1} = BlockChainServer.mine_block("1", tx1) # mine block
-  #   BlockChainServer.add_block("1",block1) # add block to blockchain
+    {:ok, charlie_sk} = RsaEx.generate_private_key
+    {:ok, charlie_pk} = RsaEx.generate_public_key(charlie_sk)
 
-  #   {:ok, block2} = BlockChainServer.mine_block("1", tx2)
-  #   BlockChainServer.add_block("1",block2)
+    {:ok, alice_sk} = RsaEx.generate_private_key
+    {:ok, alice_pk} = RsaEx.generate_public_key(alice_sk)
 
-  #   {:ok, block3} = BlockChainServer.mine_block("1", tx3)
-  #   BlockChainServer.add_block("1",block3)
+    tx1 = Transaction.init(bob_pk, alice_pk, 10)
+    tx2 = Transaction.init(alice_pk, charlie_pk, 20)
+    tx3 = Transaction.init(bob_pk, charlie_pk, 15)
+    # tx1 = ["from: Alice, to: Bob, amount = 10"]
+    # tx2 = ["seller: Alice, buyer: Charlie, amount = 20"]
+    # tx3 = ["seller: Bob, buyer: Charlie, amount = 15"]
+
+    {:ok, block1} = BlockChainServer.mine_block("1", tx1) # mine block
+    BlockChainServer.add_block("1",block1) # add block to blockchain
+
+    {:ok, block2} = BlockChainServer.mine_block("1", tx2)
+    BlockChainServer.add_block("1",block2)
+
+    {:ok, block3} = BlockChainServer.mine_block("1", tx3)
+    BlockChainServer.add_block("1",block3)
 
 
-  #   assert BlockChainServer.get_lenght_of_chain("1") == {:ok, 4} # genesis block is added upon initialization
+    assert BlockChainServer.get_lenght_of_chain("1") == {:ok, 4} # genesis block is added upon initialization
 
-  # end
+  end
 
-  # test "correctness of hashes" do
-  #   tx = ["seller: Bob, buyer: Charlie, amount = 10"]
-  #   {:ok, block} = BlockChainServer.mine_block("1", tx)
-  #   BlockChainServer.add_block("1",block)
+  test "correctness of hashes" do
+    {:ok, bob_sk} = RsaEx.generate_private_key
+    {:ok, bob_pk} = RsaEx.generate_public_key(bob_sk)
 
-  #   {:ok, block} = BlockChainServer.get_latest_block("1")
-  #   data = Map.fetch!(block, :data)
-  #   previous_hash = Map.fetch!(block, :previous_hash)
-  #   nonce = Map.fetch!(block, :nonce)
+    {:ok, charlie_sk} = RsaEx.generate_private_key
+    {:ok, charlie_pk} = RsaEx.generate_public_key(charlie_sk)
 
-  #   difficulty = 2
-  #   target = String.duplicate("0", difficulty)
-  #   calculated_hash = [data, previous_hash, Kernel.inspect(nonce)]
-  #   |> Utils.calculate_hash
+    tx = Transaction.init(charlie_pk, bob_pk, 10)
+    #tx = ["seller: Bob, buyer: Charlie, amount = 10"]
+    {:ok, block} = BlockChainServer.mine_block("1", tx)
+    BlockChainServer.add_block("1",block)
 
-  #   assert String.slice(calculated_hash, 0..difficulty-1) == target
+    {:ok, block} = BlockChainServer.get_latest_block("1")
+    data = Map.fetch!(block, :data)
+    previous_hash = Map.fetch!(block, :previous_hash)
+    nonce = Map.fetch!(block, :nonce)
 
-  # end
+    difficulty = 2
+    target = String.duplicate("0", difficulty)
+    calculated_hash = [data, previous_hash, Kernel.inspect(nonce)]
+    |> Utils.calculate_hash
 
-  # test "calculated hash is same as stored hash" do
-  #   tx = ["seller: Bob, buyer: Charlie, amount = 10"]
-  #   {:ok, block} = BlockChainServer.mine_block("1", tx)
-  #   BlockChainServer.add_block("1",block)
+    assert String.slice(calculated_hash, 0..difficulty-1) == target
 
-  #   {:ok, block} = BlockChainServer.get_latest_block("1")
-  #   data = Map.fetch!(block, :data)
-  #   previous_hash = Map.fetch!(block, :previous_hash)
-  #   nonce = Map.fetch!(block, :nonce)
-  #   hash = Map.fetch!(block, :hash)
-  #   calculated_hash = [data, previous_hash, Kernel.inspect(nonce)]
-  #   |> Utils.calculate_hash
+  end
 
-  #   assert hash == calculated_hash
+  test "calculated hash is same as stored hash" do
+    {:ok, bob_sk} = RsaEx.generate_private_key
+    {:ok, bob_pk} = RsaEx.generate_public_key(bob_sk)
 
-  # end
+    {:ok, charlie_sk} = RsaEx.generate_private_key
+    {:ok, charlie_pk} = RsaEx.generate_public_key(charlie_sk)
+
+    tx = Transaction.init(charlie_pk, bob_pk, 10)
+    # tx = ["seller: Bob, buyer: Charlie, amount = 10"]
+    {:ok, block} = BlockChainServer.mine_block("1", tx)
+    BlockChainServer.add_block("1",block)
+
+    {:ok, block} = BlockChainServer.get_latest_block("1")
+    data = Map.fetch!(block, :data)
+    previous_hash = Map.fetch!(block, :previous_hash)
+    nonce = Map.fetch!(block, :nonce)
+    hash = Map.fetch!(block, :hash)
+    calculated_hash = [data, previous_hash, Kernel.inspect(nonce)]
+    |> Utils.calculate_hash
+
+    assert hash == calculated_hash
+
+  end
 
   test "transaction signature and verification" do
     {:ok, bob_sk} = RsaEx.generate_private_key
